4c2b2e07ba49b41bf00c6ac0ef4ce1476f43156a
sbuteryd@qq.com <sbuteryd@qq.com>
2019-03-24 19:31:46 +0800

    3-2

--- index/index.js
+++ index/index.js
@@ -1,1 +1,14 @@
-Page({})
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data:{
+        city:'厦门',
+      },
+      success:(res)=>{
+        console.log(res)
+      }
+    })
+  }
+})
+
